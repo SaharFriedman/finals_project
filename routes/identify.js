4c67164dd292f38587be8cdb92ed4bad3ab7c037
saharooly <1999sahar@gmail.com>
2025-04-01 19:32:22 +0300

    commit

--- routes/identify.js
+++ routes/identify.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const identify = require('../controllers/identify');
 var router = express.Router();
-//We are using the identify controller.
-//We are using the get method to get the identification.
-console.log("hellllllloooo1");
-router.get('/', identify.getIdentification);
+router.post('/', identify.getIdentification);
 module.exports = router;
--- services/identify.js
+++ services/identify.js
@@ -1,11 +1,11 @@
 const User = require('../models/user');
-const create = async (req,res) =>{
+const create = async (req) =>{
     try{
-        const user = new User.create(req);
+        const user = new User(req.body);
         return await user.save();
     }
     catch(err){
-        return res.status(err.status).send(err.message);
+        throw Object.assign(new Error('failed to save user'), { status: 400 });
     }
 }
 module.exports = { create };
